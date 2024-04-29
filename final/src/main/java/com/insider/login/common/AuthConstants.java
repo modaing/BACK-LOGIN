e7aa00216598d8d42ca12ac8210982b9e5276a0e
jeehwan98 <152130528+jeehwan98@users.noreply.github.com>
2024-04-29 22:58:28 +0900

    Update AuthConstants.java

---
+++ final/src/main/java/com/insider/login/common/AuthConstants.java
@@ -0,0 +1,8 @@
+/*
+* JWT 관련 상수 필드 (place where variables are commonly used)
+* */
+public class AuthConstants {
+
+    public static final String AUTH_HEADER = "Authorization";
+    public static final String TOKEN_TYPE = "BEARER";
+}
