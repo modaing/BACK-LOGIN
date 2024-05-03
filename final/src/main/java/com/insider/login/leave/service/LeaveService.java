e88c503b4751340e42c194780757eed11b847a3c
antmrhdqn <antmrhdqn@gmail.com>
2024-05-03 09:29:50 +0900

    휴가 서비스 코드 주석 삭제, 정리

--- final/src/main/java/com/insider/login/leave/service/LeaveService.java
+++ final/src/main/java/com/insider/login/leave/service/LeaveService.java
@@ -40,193 +40,128 @@ public class LeaveService extends LeaveUtil {
     }
 
     public Page<LeaveSubmitDTO> selectLeaveSubmitList(int applicantId, Pageable pageable) {
+        try {
+            // 조건에 따라 휴가신청내역을 가져옴
+            Page<LeaveSubmit> submitList = null;
+            if (applicantId > 0) {
+                submitList = leaveSubmitRepository.findByMemberId(applicantId, pageable);
+            } else {
+                submitList = leaveSubmitRepository.findAll(pageable);
+            }
 
-        log.info("[신청내역] 휴가 신청 내역 확인 시작 ======================================================");
+            List<LeaveSubmitDTO> DTOList = new ArrayList<>();
+            for (LeaveSubmit submit : submitList) {
+                LeaveSubmitDTO leaveSubmitDTO = modelMapper.map(submit, LeaveSubmitDTO.class);
 
-        // 조건에 따라 휴가신청내역을 가져옴
-        Page<LeaveSubmit> submitList = null;
-        if (applicantId > 0) {
-            submitList = leaveSubmitRepository.findByMemberId(applicantId, pageable);
-        } else {
-            submitList = leaveSubmitRepository.findAll(pageable);
-        }
-
-        List<LeaveSubmitDTO> DTOList = new ArrayList<>();
-
-        for (LeaveSubmit submit : submitList) {
-            LeaveSubmitDTO leaveSubmitDTO = modelMapper.map(submit, LeaveSubmitDTO.class);
-
-            // 사번으로 사원명 조회해서 DTO에 넣기 ( member 안정화되면 추가 예정 )
+                // TODO:사번으로 사원명 조회해서 DTO에 넣기 ( member 안정화되면 추가 예정 )
 //            leaveSubmitDTO.setApplicantName(memberRepository.findMemberNameByMemberId(applicantId));
 
-            // 승인자 사번이 존재할 경우 승인자 사번으로 사원명을 조회해서 DTO에 넣기 ( member 안정화 후 추가 예정 )
+                // TODO:승인자 사번이 존재할 경우 승인자 사번으로 사원명을 조회해서 DTO에 넣기 ( member 안정화 후 추가 예정 )
 //            if (submit.getLeaveSubApprover() != 0) {
 //                leaveSubmitDTO.setApproverName(memberRepository.findById(submit.getLeaveSubApprover()));
 //            }
 
-            DTOList.add(leaveSubmitDTO);
+                DTOList.add(leaveSubmitDTO);
+            }
+
+            // DTOList를 기존 pageable 정보를 가진 새로운 페이지로 만들어서 반환
+            return new PageImpl<>(DTOList, submitList.getPageable(), submitList.getTotalElements());
+        } catch (Exception e) {
+            return Page.empty();
         }
-
-        // DTO로 변환된 내역들을 새로운 페이지로 만들어서 반환
-        return new PageImpl<>(DTOList, submitList.getPageable(), submitList.getTotalElements());
-
     }
 
     @Transactional
     public String insertSubmit(LeaveSubmitDTO leaveSubmitDTO) {
-        log.info("[휴가신청] 시작 ================================");
-
-        leaveSubmitDTO.setLeaveSubStatus("대기");
-
-        int result = 0;
-
         try {
-
-            log.info("[dto 확인] ============================\n" + leaveSubmitDTO);
+            leaveSubmitDTO.setLeaveSubStatus("대기");
 
             LeaveSubmit leaveSubmit = modelMapper.map(leaveSubmitDTO, LeaveSubmit.class);
 
-            log.info("[휴가신청] 엔티티 확인 =======================\n" + leaveSubmit);
-
-
             leaveSubmitRepository.save(leaveSubmit);
-            result = 1;
 
+            return "신청 등록 성공";
         } catch (Exception e) {
-            log.info("[휴가신청] Exception!");
-
-            result = 0;
+            return "신청 등록 실패";
         }
-
-        return (result > 0) ? "신청 등록 성공" : "신청 등록 실패";
     }
 
     @Transactional
     public String deleteSubmit(int leaveSubNo) {
-        log.info("[신청취소] 시작 ===================================");
-
-        int result = 0;
-
         try {
-
             leaveSubmitRepository.deleteById(leaveSubNo);
 
-            result = 1;
-
+            return "신청 취소 성공";
         } catch (Exception e) {
-            log.info("[신청취소] Exception!");
-
-            result = 0;
+            return "신청 취소 실패";
         }
-
-        return (result > 0) ? "신청 취소 성공" : "신청 취소 실패";
     }
 
     public String insertSubmitCancel(LeaveSubmitDTO DTO) {
-        log.info("[취소요청] 시작 ====================================");
-
-        // 화면에서 받아온 정보를 취소 요청 등록 폼으로 수정
-        LeaveSubmitDTO leaveSubmitDTO = new LeaveSubmitDTO(DTO.getLeaveSubNo(), DTO.getLeaveSubApplicant(), DTO.getLeaveSubStartDate(), DTO.getLeaveSubEndDate(), DTO.getLeaveSubApplyDate(), "취소", "대기", DTO.getLeaveSubReason());
-        log.info("[취소요청] dto 확인 ==============================\n" + leaveSubmitDTO);
-
-        int result = 0;
-
         try {
+            // 화면에서 받아온 정보를 취소 요청 등록 폼으로 수정
+            LeaveSubmitDTO leaveSubmitDTO = new LeaveSubmitDTO(DTO.getLeaveSubNo(), DTO.getLeaveSubApplicant(), DTO.getLeaveSubStartDate(), DTO.getLeaveSubEndDate(), DTO.getLeaveSubApplyDate(), "취소", "대기", DTO.getLeaveSubReason());
 
             LeaveSubmit leaveSubmit = modelMapper.map(leaveSubmitDTO, LeaveSubmit.class);
 
-            log.info("[취소요청] 엔티티 확인 ==============================\n" + leaveSubmit);
             leaveSubmitRepository.save(leaveSubmit);
 
-            result = 1;
+            return "취소요청 등록 성공";
         } catch (Exception e) {
-
-            log.info("[취소요청] Exception!");
-
-            result = 0;
-
+            return "취소요청 등록 실패";
         }
-
-        return (result > 0) ? "취소요청 등록 성공" : "취소요청 등록 실패";
     }
 
     public Page<LeaveAccrualDTO> selectAccrualList(Pageable pageable) {
+        try {
+            Page<LeaveAccrual> accrualList = leaveAccrualRepository.findAll(pageable);
 
-        log.info("[발생내역] 발생내역 조회 시작 ========================================");
-        // 페이징 처리를 위해 pageable 변수 선언
+            List<LeaveAccrualDTO> DTOList = new ArrayList<>();
+            if (accrualList != null) {
 
-        Page<LeaveAccrual> accrualList = leaveAccrualRepository.findAll(pageable);
-        log.info("[발생내역] 엔티티 리스트 확인 ==============================");
-        accrualList.forEach(System.out::println);
+                for (LeaveAccrual accrual : accrualList) {
+                    DTOList.add(modelMapper.map(accrual, LeaveAccrualDTO.class));
+                }
 
-        List<LeaveAccrualDTO> DTOList = new ArrayList<>();
-
-        if (accrualList != null) {
-
-            for (LeaveAccrual accrual : accrualList) {
-                DTOList.add(modelMapper.map(accrual, LeaveAccrualDTO.class));
+                // DTOList를 기존 pageable 정보를 가진 새로운 페이지로 만들어서 반환
+                return new PageImpl<>(DTOList, accrualList.getPageable(), accrualList.getTotalElements());
+            } else {
+                return Page.empty();
             }
-
-            return new PageImpl<>(DTOList, accrualList.getPageable(), accrualList.getTotalElements());
-
-        } else {
+        } catch (Exception e) {
             return Page.empty();
         }
     }
 
     @Transactional
     public String insertAccrual(LeaveAccrualDTO accrualDTO) {
-
-        int result = 0;
-
         try {
-
             LeaveAccrual leaveAccrual = modelMapper.map(accrualDTO, LeaveAccrual.class);
-            log.info("[발생등록] 엔티티 확인 ============================\n" + leaveAccrual);
 
             leaveAccrualRepository.save(leaveAccrual);
 
-            result = 1;
+            return "휴가발생 등록 성공";
         } catch (Exception e) {
-
-            log.info("[발생등록] Exception!");
-
-            result = 0;
-
+            return "휴가발생 등록 실패";
         }
-
-        return (result > 0) ? "휴가발생 등록 성공" : "휴가발생 등록 실패";
     }
 
 
     public LeaveSubmitDTO selectSubmitByLeaveSubNo(int leaveSubNo) {
-
-        log.info("휴가 상세 조회 시작 ===================================");
-
-        LeaveSubmitDTO leaveSubmitDTO = null;
-
         try {
             Optional<LeaveSubmit> leaveSubmit = leaveSubmitRepository.findById(leaveSubNo);
 
-            log.info("엔티티 확인 ======================\n" + leaveSubmit);
+            LeaveSubmitDTO leaveSubmitDTO = modelMapper.map(leaveSubmit, LeaveSubmitDTO.class);
 
-            leaveSubmitDTO = modelMapper.map(leaveSubmit, LeaveSubmitDTO.class);
-
-            log.info("DTO 확인\n" + leaveSubmitDTO);
-
+            return leaveSubmitDTO;
 
         } catch (Exception e) {
-            log.info("예외발생 =============================");
+            return null;
         }
-
-        return leaveSubmitDTO;
     }
 
     @Transactional
     public String updateSubmit(LeaveSubmitDTO leaveSubmitDTO) {
-        log.info("[휴가처리] 시작 ===============================");
-        int result = 0;
-
         try {
             // 업데이트할 신청 번호로 해당 휴가 신청 정보 조회
             Optional<LeaveSubmit> leaveSubmit = leaveSubmitRepository.findById(leaveSubmitDTO.getLeaveSubNo());
@@ -246,29 +181,21 @@ public class LeaveService extends LeaveUtil {
             LeaveSubmit newSubmit = modelMapper.map(tempDTO, LeaveSubmit.class);
             leaveSubmitRepository.save(newSubmit);
 
-            result = 1;
-
+            return "휴가처리 성공";
         } catch (Exception e) {
-            log.info("[휴가처리] 예외발생 =============================");
-            result = 0;
-
+            return "휴가처리 실패";
         }
-
-        return (result > 0) ? "휴가처리 성공" : "휴가처리 실패";
     }
 
     public Page<LeaveInfoDTO> selectLeavesList(Pageable pageable) {
-        log.info("[보유내역] 시작 ==========================================");
         try {
-
             Page<Leaves> leavesPage = leaveRepository.findAll(pageable);
 
+            // Collectors.groupingBy()를 통해 Map 타입으로 반환, 괄호 안은 그룹화 기준이자 키
             Map<Integer, List<Leaves>> leavesByMemberId = leavesPage.stream()
-                    // Collectors.groupingBy()를 통해 Map 타입으로 반환, 괄호 안은 그룹화 기준이자 키
                     .collect(Collectors.groupingBy(Leaves::getMemberId));
 
             List<LeaveInfoDTO> infoDTOList = new ArrayList<>();
-
             for (Map.Entry<Integer, List<Leaves>> entry : leavesByMemberId.entrySet()) {
 
                 // 밸류에는 하나의 사번이 가진 모든 휴가내역을 list로 가지고 있음
@@ -278,16 +205,13 @@ public class LeaveService extends LeaveUtil {
                 infoDTOList.add(addLeaveInfo(info));
             }
 
-            // 리스트를 페이지로 전환
+            // DTOList를 기존 pageable 정보를 가진 새로운 페이지로 만들어서 반환
             Page<LeaveInfoDTO> infoDTOPage = new PageImpl<>(infoDTOList, pageable, leavesPage.getTotalElements());
 
             return infoDTOPage;
         } catch (Exception e) {
-
-            log.info("[보유내역] 에러 ======================================");
+            return null;
         }
-
-        return null;
     }
 
     // 사번을 매개변수로 주면 해당 사원의 휴가 정보를 DTO로 반환하는 메소드
@@ -296,7 +220,6 @@ public class LeaveService extends LeaveUtil {
         List<Leaves> leavesList = leaveRepository.findByMemberId(memberId);
 
         LeaveInfoDTO info = leaveInfoCalc(leavesList);
-        log.info("휴가 보유 조회 ===========\n" + info);
 
         return addLeaveInfo(info);
     }
