5df068a4ed0f4f6f12963e3b087bc39d59e85a5f
antmrhdqn <antmrhdqn@gmail.com>
2024-05-08 20:13:27 +0900

    수요조사 등록 버그 수정

---
+++ final/src/main/java/com/insider/login/other/survey/dto/SurveyInsertRequestDTO.java
@@ -0,0 +1,40 @@
+package com.insider.login.other.survey.dto;
+
+import java.util.List;
+
+public class SurveyInsertRequestDTO {
+    private SurveyDTO surveyDTO;
+    private List<String> answers;
+
+    public SurveyInsertRequestDTO() {
+    }
+
+    public SurveyInsertRequestDTO(SurveyDTO surveyDTO, List<String> answers) {
+        this.surveyDTO = surveyDTO;
+        this.answers = answers;
+    }
+
+    public SurveyDTO getSurveyDTO() {
+        return surveyDTO;
+    }
+
+    public void setSurveyDTO(SurveyDTO surveyDTO) {
+        this.surveyDTO = surveyDTO;
+    }
+
+    public List<String> getAnswers() {
+        return answers;
+    }
+
+    public void setAnswers(List<String> answers) {
+        this.answers = answers;
+    }
+
+    @Override
+    public String toString() {
+        return "SurveyInsertRequestDTO{" +
+                "surveyDTO=" + surveyDTO +
+                ", answers=" + answers +
+                '}';
+    }
+}
