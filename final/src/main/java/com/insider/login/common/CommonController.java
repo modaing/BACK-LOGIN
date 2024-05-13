69ea678b9c73b0655b5b2a5d752324135257d743
her9797 <simkoon0120@gmail.com>
2024-05-13 09:57:03 +0900

    Note 정렬 수정

--- final/src/main/java/com/insider/login/common/CommonController.java
+++ final/src/main/java/com/insider/login/common/CommonController.java
@@ -12,7 +12,7 @@ public class CommonController {
     /** 페이징 관련 DATA 가져오는 메서드 */
     public static Pageable getPageable(int page, int size, String sort, String direction) {
         Sort pageableSort;
-        if ("ancNo".equals(sort)) {
+        if ("ancNo".equals(sort) || "noteNo".equals(sort)) {
             Sort.Direction sortDirection;
             try {
                 sortDirection = Sort.Direction.fromString(direction);
@@ -20,12 +20,19 @@ public class CommonController {
                 // direction 값이 잘못된 경우 기본값인 DESC로 설정
                 sortDirection = Sort.Direction.DESC;
             }
-            pageableSort = Sort.by(sortDirection, "ancNo");
+
+            if ("noteNo".equals(sort)) {
+                pageableSort = Sort.by(sortDirection, "noteNo");
+            } else { // "ancNo"인 경우
+                pageableSort = Sort.by(sortDirection, "ancNo");
+            }
+
         } else {
             pageableSort = Sort.unsorted();
         }
         return PageRequest.of(page, size, pageableSort);
     }
+
 
     /** 현재 시간 가져오는 메서드 */
     public static String nowDate () {
