1a2e6afe0aba5366a3baf28f543d420954ae4661
antmrhdqn <antmrhdqn@gmail.com>
2024-05-16 13:13:34 +0900

    컨트롤러 로그 임포트 수정

--- final/src/main/java/com/insider/login/leave/controller/LeaveController.java
+++ final/src/main/java/com/insider/login/leave/controller/LeaveController.java
@@ -8,6 +8,7 @@ import com.insider.login.leave.dto.LeaveSubmitDTO;
 import com.insider.login.leave.entity.LeaveSubmit;
 import com.insider.login.leave.service.LeaveService;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
