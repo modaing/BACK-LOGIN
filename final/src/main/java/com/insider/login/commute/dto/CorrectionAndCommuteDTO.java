d3815eb2c898819d6319cec86caba193cc94ce4e
jeonghee <hongi_@naver.com>
2024-05-22 12:38:46 +0900

    fix #26 - [출퇴근] 출퇴근 내역 조회 /정정 내역 조회 중 JPA 쿼리 오류 이슈

---
+++ final/src/main/java/com/insider/login/commute/dto/CorrectionAndCommuteDTO.java
@@ -0,0 +1,34 @@
+package com.insider.login.commute.dto;
+
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class CorrectionAndCommuteDTO {
+
+//    private int commuteNo;                  // 출퇴근 번호
+//    private int memberId;                   // 사번
+//    private LocalDate workingDate;          // 근무 일자
+//    private LocalTime startWork;            // 출근 시간
+//    private LocalTime endWork;              // 퇴근 시간
+//    private String workingStatus;           // 근무 상태
+//    private int totalWorkingHours;          // 총 근무 시간
+//    private int corrNo;                         // 정정 요청 번호
+//    private String reqStartWork;                // 정정 요청 출근 시간
+//    private String reqEndWork;                  // 정정 요청 퇴근 시간
+//    private String reasonForCorr;               // 정정 사유
+//    private LocalDate corrRegistrationDate;     // 정정 등록 일자
+//    private String corrStatus;                  // 정정 상태
+//    private String reasonForRejection;          // 반려 사유
+//    private LocalDate corrProcessingDate;       // 정정 처리 일자
+
+    private CorrectionDTO correction;       // 출퇴근 정정
+    private CommuteDTO commute;             // 출퇴근
+
+}
