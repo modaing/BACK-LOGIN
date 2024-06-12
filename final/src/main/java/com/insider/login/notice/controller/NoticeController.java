eac7350af6a41c25bd6d975d55421db11ed439a7
Jeonghee Kim <hongi_@naver.com>
2024-06-12 18:33:14 +0900

    feat #2 - [알림] SSE 알림 기능을 위해 컨트롤러와 서비스 로직 추가

--- final/src/main/java/com/insider/login/notice/controller/NoticeController.java
+++ final/src/main/java/com/insider/login/notice/controller/NoticeController.java
@@ -2,9 +2,11 @@ package com.insider.login.notice.controller;
 
 import com.insider.login.common.ResponseMessage;
 import com.insider.login.notice.dto.NoticeDTO;
+import com.insider.login.notice.entity.Notice;
 import com.insider.login.notice.service.NoticeService;
 import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -21,16 +23,7 @@ public class NoticeController {
         this.noticeService = noticeService;
     }
 
-    @GetMapping(value = "/notices/subscribe/{id}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public SseEmitter subscribe(@PathVariable Long id) {
-        return noticeService.subscribe(id);
-    }
-
-    @PostMapping("/notices/send-data/{id}")
-    public void sendData(@PathVariable Long id) {
-        noticeService.notify(id, "data");
-    }
-
+    /** 일반적인 HTTP 요청/응답 방식으로 구현한 알림 (실시간 기능X) */
     /** 새로운 알림 수신 (등록) */
     @PostMapping("/notices")
     public ResponseEntity<ResponseMessage> insertNoticeOfReqForCorrect(@RequestBody NoticeDTO newNotice) {
@@ -55,4 +48,26 @@ public class NoticeController {
         return ResponseEntity.ok().body(new ResponseMessage(200, "전체 삭제 성공", noticeService.deleteNoticeListByMemberId(memberId)));
     }
 
+    /** SSE(Server-Sent-Events) 방식으로 구현한 실시간 알림 */
+
+    //    @GetMapping(value = "/notices/subscribe/{id}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+//    public SseEmitter subscribe(@PathVariable Long id) {
+//        return noticeService.subscribe(id);
+//    }
+//
+//    @PostMapping("/notices/send-data/{id}")
+//    public void sendData(@PathVariable Long id) {
+//        noticeService.notify(id, "data");
+//    }
+
+    @GetMapping("/notices")
+    public SseEmitter subscribe() {
+        return noticeService.subscribe();
+    }
+
+    public ResponseEntity<Notice> createNotice(@RequestBody Notice notice) {
+        Notice savedNotice = noticeService.createNotice(notice);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedNotice);
+    }
 }
--- final/src/main/java/com/insider/login/notice/service/NoticeService.java
+++ final/src/main/java/com/insider/login/notice/service/NoticeService.java
@@ -1,17 +1,23 @@
 package com.insider.login.notice.service;
 
+import com.insider.login.member.entity.Member;
 import com.insider.login.notice.dto.NoticeDTO;
 import com.insider.login.notice.entity.Notice;
 import com.insider.login.notice.repository.NoticeRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
 @Service
@@ -20,6 +26,7 @@ public class NoticeService {
 
     private final NoticeRepository noticeRepository;
     private final ModelMapper modelMapper;
+    private final Map<Integer, List<SseEmitter>> subscribers = new ConcurrentHashMap<>();
 
     public NoticeService(NoticeRepository noticeRepository,
                          ModelMapper modelMapper) {
@@ -27,6 +34,9 @@ public class NoticeService {
         this.modelMapper = modelMapper;
     }
 
+    /**
+     * 일반적인 HTTP 요청/응답 방식으로 구현한 알림 (실시간 기능X)
+     */
     @Transactional
     public Map<String, Object> insertNewNotice(NoticeDTO newNotice) {
         /** 새로운 알림 수신 (등록) */
@@ -88,9 +98,49 @@ public class NoticeService {
         return result;
     }
 
-    public SseEmitter subscribe(Long id) {
+    /**
+     * SSE(Server-Sent-Events) 방식으로 구현한 실시간 알림
+     */
+
+//    public SseEmitter subscribe(Long id) {
+//    }
+//
+//    public void notify(Long id, String data) {
+//    }
+    public SseEmitter subscribe() {
+//        UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+//        int memberId = userDetails.getAuthorities().size();
+        Member member = (Member) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        int memberId = member.getMemberId();
+
+        SseEmitter emitter = new SseEmitter();
+
+        List<SseEmitter> memberSubscribers = subscribers.computeIfAbsent(memberId, k -> new ArrayList<>());
+        memberSubscribers.add(emitter);
+
+        emitter.onCompletion(() -> memberSubscribers.remove(emitter));
+        emitter.onTimeout(() -> memberSubscribers.remove(emitter));
+
+        return emitter;
     }
 
-    public void notify(Long id, String data) {
+    public Notice createNotice(Notice notice) {
+        Notice savedNotice = noticeRepository.save(notice);
+        notifySubscribers(savedNotice);
+
+        return savedNotice;
+    }
+
+    private void notifySubscribers(Notice notice) {
+        List<SseEmitter> memberSubscribers = subscribers.get(notice.getMemberId());
+        if (memberSubscribers != null) {
+            for (SseEmitter emitter : memberSubscribers) {
+                try {
+                    emitter.send(notice);
+                } catch (IOException e) {
+                    memberSubscribers.remove(emitter);
+                }
+            }
+        }
     }
 }
